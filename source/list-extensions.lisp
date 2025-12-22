cbafdc47ffcce4ccffac1238246c2d1ab93254e1
daedsidog <41439659+daedsidog@users.noreply.github.com>
2025-12-22 23:29:56 +0200

    Convert CL:LOOP keywords from symbols to keyword symbols

--- source/list-extensions.lisp
+++ source/list-extensions.lisp
@@ -11,30 +11,30 @@
   "Return a list of duplicate elements in DATA-LIST."
   (check-type data-list list)
   (let ((hash (make-hash-table :test test)))
-    (loop for x in data-list
-          when (= (incf (gethash x hash 0)) 2)
-          collect x into dupes
-          finally (return dupes))))
+    (loop :for x :in data-list
+          :when (= (incf (gethash x hash 0)) 2)
+          :collect x :into dupes
+          :finally (return dupes))))
 
 (defun unique (data-list &optional &key (test #'eq))
   "Return a list of unique elements in DATA-LIST."
   (let ((hash (make-hash-table :test test)))
-    (loop for x in data-list
-          when (= (incf (gethash x hash 0)) 1)
-          collect x into uniques
-          finally (return uniques))))
+    (loop :for x :in data-list
+          :when (= (incf (gethash x hash 0)) 1)
+          :collect x :into uniques
+          :finally (return uniques))))
 
 (defun cars (list)
   "Return the CAR of each nonempty list in LIST."
   (let ((is-first-iteration t))
-    (let ((cars (loop for item in list
-                      if (listp item)
-                      collect (nreverse (cars item)) into cars
-                      else
-                      when is-first-iteration
-                      collect item into cars
-                      and do (setf is-first-iteration nil)
-                      finally (return cars))))
+    (let ((cars (loop :for item :in list
+                      :if (listp item)
+                        :collect (nreverse (cars item)) :into cars
+                      :else
+                        :when is-first-iteration
+                          :collect item :into cars
+                          :and :do (setf is-first-iteration nil)
+                      :finally (return cars))))
       (flatten cars))))
 
 (defun recursive-mapl (fn list)
@@ -43,9 +43,9 @@
 Recursive version of CL:MAPL."
   (check-type fn function)
   (flet ((helper (list)
-           (loop for item in list
-                 when (listp item)
-                   do (recursive-mapl fn item)
-                      (mapl fn item))))
+           (loop :for item :in list
+                 :when (listp item)
+                   :do (recursive-mapl fn item)
+                       (mapl fn item))))
     (helper list))
   list)
--- source/string-extensions.lisp
+++ source/string-extensions.lisp
@@ -7,18 +7,17 @@
 (in-package #:ck-clle/string-extensions)
 
 (defun string-empty-p (string)
-  (not (loop for char across string
-               thereis char)))
+  (not (loop :for char :across string :thereis char)))
 
 (defun indent-string (string indentation-string)
   "Prepend INDENTATION-STRING to each line in STRING."
   (with-output-to-string (out)
     (with-input-from-string (in string)
       (let ((is-first-line t))
-        (loop for line = (read-line in nil nil)
-              while line
-              do (if is-first-line
-                   (setf is-first-line nil)
-                   (format out "~%"))
-                 (format out "~A~A" indentation-string line))))
+        (loop :for line = (read-line in nil nil)
+              :while line
+              :do (if is-first-line
+                    (setf is-first-line nil)
+                    (format out "~%"))
+                  (format out "~A~A" indentation-string line))))
     out))
