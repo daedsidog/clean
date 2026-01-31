f325756ce8b6f3c6dae387302a582aa0e7c9dd17
daedsidog <41439659+daedsidog@users.noreply.github.com>
2026-01-31 02:20:55 +0200

    package-extensions: Add SETF expanders to package nickname functions

--- source/package-extensions.lisp
+++ source/package-extensions.lisp
@@ -4,16 +4,27 @@
   (:use #:cl)
   (:import-from #:trivial-package-local-nicknames
                 #:add-package-local-nickname)
-  (:export #:set-package-nicknames
-           #:set-package-local-nicknames))
+  (:export #:package-local-nicknames))
 
 (in-package #:ck-clle/package-extensions)
 
-(defun set-package-nicknames (package nicknames)
+#+sbcl
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (sb-ext:unlock-package :common-lisp))
+
+(defun (setf package-nicknames) (nicknames package)
   "Return the package renamed after setting global NICKNAMES for PACKAGE."
   (rename-package package package nicknames))
 
-(defun set-package-local-nicknames (package nicknames-alist)
+(defun package-local-nicknames (package)
+  "Return the alist of local nicknames for PACKAGE."
+  (trivial-package-local-nicknames:package-local-nicknames package))
+
+(defun (setf package-local-nicknames) (nicknames-alist package)
   "Set package-local nicknames for PACKAGE from NICKNAMES-ALIST."
   (loop :for (nickname . actual-package) :in nicknames-alist
         :do (add-package-local-nickname nickname actual-package package)))
+
+#+sbcl
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (sb-ext:lock-package :common-lisp))
