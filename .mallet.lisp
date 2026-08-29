f46893d287bfd6731fe431f81010d409f7f082e8
daedsidog <41439659+daedsidog@users.noreply.github.com>
2026-08-29 11:47:26 +0300

    Revert Mallet float marker enforcement

--- .mallet.lisp
+++ .mallet.lisp
@@ -1,4 +1,5 @@
 (:mallet-config
   (:extends :default)
   (:enable :line-length :max 100)
-  (:enable :no-package-use :allow ("CLEAN" "CLEAN/ALIASES")))
+  (:enable :no-package-use :allow ("CLEAN" "CLEAN/ALIASES"))
+  (:disable :bare-float-literal))
